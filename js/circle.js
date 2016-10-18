4f49ef7fc5b15a051ff67444605f3d010f22d954
Humberto Sandigo <sandigohumberto@gmail.com>
2016-10-18 10:21:43 -0500

    debug exercise and calculator exercise

--- js/circle.js
+++ js/circle.js
@@ -4,27 +4,22 @@
     // create a circle object
     var circle = {
         radius: 3,
-
-        getArea: function (pi * this.radius^2) {
-            // TODO: complete this method
+        getArea: function () {
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
-        }
+            return Math.PI * Math.pow(this.radius, 2);
+        },
         logInfo: function (doRounding) {
             // TODO: complete this method.
             var area = this.getArea();
-            if(doRounding){
-                Math.round(area);
-            }else{
-                return area;
+            if(doRounding){ // if statements expect boolean values
+                area = Math.round(area);
             }
-            // If doRounding is true, round the result to the nearest integer.
+            // If doRounding is true, round the area to the nearest integer.
             // Otherwise, output the complete value
 
             console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
-    }
+    };
 
     // log info about the circle
     console.log("Raw circle information");
@@ -33,8 +28,7 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5; // to get back inside the object you have to use the identifier of circle.radius
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
